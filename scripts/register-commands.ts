94de7caf7d4b5d10ce7020cd50b6065ca8a0085c
jill64 <contact@jill64.dev>
2025-03-03 03:05:16 +0900

    setup

---
+++ scripts/register-commands.ts
@@ -0,0 +1,24 @@
+import { REST, Routes } from 'discord.js'
+import 'dotenv/config'
+import { env } from 'node:process'
+
+const commands = [
+  {
+    name: 'register',
+    description: '今入っているVCを測定対象にするよ！'
+  },
+  {
+    name: 'unregister',
+    description: '今入っているVCを測定対象から外すよ！'
+  },
+  {
+    name: 'show',
+    description: 'あなたの累計作業時間を表示するよ！'
+  }
+]
+
+const rest = new REST({ version: '10' }).setToken(env.DISCORD_BOT_TOKEN!)
+
+await rest.put(Routes.applicationCommands(env.DISCORD_APPLICATION_ID!), {
+  body: commands
+})
